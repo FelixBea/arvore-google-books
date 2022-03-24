f7998a59abfd1d9445107557e0d3f4d94ace2225
Beatriz Felix <bfelix@live.com>
2022-03-24 20:56:17 -0300

    Adicionando redux, eslint e prettier com configurações

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "prettier",
+        "prettier/react"
+    ],
+    "parser": 'babel-eslint',
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        'import/prefer-default-export': 'off'
+    }
+}
